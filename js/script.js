e35e8857726e137affad4e0f0043a8e2356a30f3
Lorena Spiller <lorenaspiller@gmail.com>
2021-09-08 15:41:15 +0200

    basic dragging-slider

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        
+        dots: true,
+        // draggable: true,
+        // swipe: true
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        arrows: true,
+        appendDots: $('.slider-image'),
+        appendArrows: $('.slider-image'),
+  });
+});
